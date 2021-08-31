e23227785a33cea12f2a5ea3ec50a03539bf0d82
cacacaio <cacaioassuncao@hotmail.com>
2021-08-30 22:11:39 -0300

    Bot Starting

--- index.ts
+++ index.ts
@@ -1,3 +1,12 @@
 require('dotenv').config()
 
+import Client from './src/bot'
+
+const client = new Client();
+client.login(process.env.TOKEN);
+client.once('ready', () =>{
+    console.log('Pai ta Pronto')
+})
+client.on('messageCreate', message =>{
+})
 console.log(process.env.TOKEN)
