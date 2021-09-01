cfa8e9a6597953c12d18df7618c23b824dc25338
Caio Augusto <cacaioassuncao@hotmail.com>
2021-09-01 12:38:36 -0300

    Music Working

--- bot.ts
+++ bot.ts
@@ -1,16 +1,17 @@
-import {BaseCommandInteraction, Client, Collection, CommandInteraction, Intents} from 'discord.js'
+import { BaseCommandInteraction, Client, Collection, CommandInteraction, Intents } from 'discord.js'
 
-import { Player } from 'discord-player';
-import { SlashCommandOptionsOnlyBuilder } from '@discordjs/builders';
+import { Player } from 'discord-player'
+import { SlashCommandOptionsOnlyBuilder } from '@discordjs/builders'
 
-export type Commands = {
-    data : SlashCommandOptionsOnlyBuilder
-    execute : (interaction : BaseCommandInteraction) => void
+export interface Commands {
+  data: SlashCommandOptionsOnlyBuilder
+  execute(interaction: CommandInteraction): void
+  execute(interaction: BaseCommandInteraction, player: Player): void
 }
-export default class extends Player{
-    commands : Collection<string, Commands>
-    constructor(client: Client){
-        super(client)
-        this.commands = new Collection<string, Commands>();
-    }
+export default class extends Player {
+  commands: Collection<string, Commands>
+  constructor(client: Client) {
+    super(client)
+    this.commands = new Collection<string, Commands>()
+  }
 }
--- index.ts
+++ index.ts
@@ -2,30 +2,47 @@ import 'dotenv/config'
 
 import { Client, Intents } from 'discord.js'
 
-import {Commands} from './bot'
+import { Commands } from './bot'
 import Player from './bot'
 import { deploy } from './deployCommands'
 import fs from 'fs'
 
 const client = new Client({
-    intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]
-});
+  intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS],
+})
 const player = new Player(client)
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js') || file.endsWith('.ts'));
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js') || file.endsWith('.ts'))
 
 for (const file of commandFiles) {
-    const command : Commands = require(`./commands/${file}`);
-    player.commands.set(command.data.name, command);
+  const command: Commands = require(`./commands/${file}`)
+  player.commands.set(command.data.name, command)
 }
-client.login(process.env.TOKEN);
-client.once('ready', () =>{
-    console.log('Pai ta Pronto')
+client.login(process.env.TOKEN)
+client.once('ready', () => {
+  console.log('Pai ta Pronto')
 })
-client.on('messageCreate', message => {
-    if(message.content == '!deploy' && message.guild && message.member?.permissions.has('ADMINISTRATOR')){
-        deploy([...player.commands.values()].map(c => c.data), message.guild.id)
-    }
+client.on('messageCreate', (message) => {
+  if (
+    message.content == '!deploy' &&
+    message.guild &&
+    message.member?.permissions.has('ADMINISTRATOR')
+  ) {
+    deploy(
+      [...player.commands.values()].map((c) => c.data),
+      message.guild.id,
+    )
+  }
 })
-client.on('interactionCreate', async interaction =>{
-
-});
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return
+  await interaction.deferReply()
+  const command = player.commands.get(interaction.commandName)
+  command?.execute(interaction, player)
+})
+player.on('botDisconnect', (queue) => {
+  if (!queue.destroyed) {
+    queue.destroy()
+  }
+})
