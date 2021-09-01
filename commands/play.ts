b6663c7137b55d540250754c78a942fdc40509f7
Caio Augusto <cacaioassuncao@hotmail.com>
2021-09-01 12:38:19 -0300

    Music Working

--- commands/play.ts
+++ commands/play.ts
@@ -5,31 +5,42 @@ import { Interaction } from 'discord.js'
 import { SlashCommandBuilder } from '@discordjs/builders'
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('play').setDescription('Tocar uma Musica'),
+  data: new SlashCommandBuilder()
+    .setName('play')
+    .setDescription('Toca musica')
+    .addStringOption((option) =>
+      option.setName('song').setRequired(true).setDescription('Nome da musica'),
+    ),
   execute: async (interaction: CommandInteraction, player: Player) => {
     if (!interaction.guildId) return
-    await interaction.deferReply()
+
     const url = interaction.options.getString('song', true)
     if (interaction.member instanceof GuildMember && interaction.member.voice.channel) {
       const channel = interaction.member.voice.channel
-      const queue = player.createQueue(interaction.guild!, { metadata: { channel: channel } })
+      const queue = player.createQueue(interaction.guild!, {
+        metadata: { channel: channel },
+      })
 
       try {
         if (!queue.connection) await queue.connect(channel)
       } catch (err) {
         console.log(err)
-        interaction.followUp('Não consegui entrar no canal')
+        await interaction.followUp('Não consegui entrar no canal')
         queue.destroy()
       }
-      const song = await player
+      const track = await player
         .search(url, {
           requestedBy: interaction.user,
           searchEngine: QueryType.AUTO,
         })
-        .catch((err) => console.log(err))
-      if (!song || !song.tracks.length) return interaction.followUp('Não consegui achar a musica')
-      song.playlist ? queue.addTracks(song.tracks) : queue.addTrack(song.tracks[0])
+        .catch((err) => {
+          console.log(err)
+          interaction.followUp({ content: 'Erro ao buscar musica', ephemeral: true })
+        })
+      if (!track || !track.tracks.length) return interaction.followUp('Não consegui achar a musica')
+      track.playlist ? queue.addTracks(track.tracks) : queue.addTrack(track.tracks[0])
       if (!queue.playing) await queue.play()
+      await interaction.editReply(`Tocando a musica **${track.tracks[0].source}**`)
     } else {
       await interaction.editReply('Voce não está em um canal!')
     }
